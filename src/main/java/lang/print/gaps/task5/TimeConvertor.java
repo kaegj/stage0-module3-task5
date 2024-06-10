a4e617ac549cf31922910a9a852e4e770ea9e514
kaegan <kkiefer4@student.umgc.edu>
2024-06-10 10:40:44 +0900

    Submission 3

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
-        float seconds = minutes / 60;
-        System.out.println(seconds);
+        System.out.println(minutes / 60.0);
     }
 }
